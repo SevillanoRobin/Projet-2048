e45c3e8d1e455684263c4e0b41765d09b4412bca
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-09-26 13:18:21 +0200

    2- Add constants.

---
+++ src/Parametres.java
@@ -0,0 +1,8 @@
+public interface Parametres {
+    static final int HAUT     = 1;
+    static final int DROITE   = 2;
+    static final int BAS      = -1;
+    static final int GAUCHE   = -2;
+    static final int TAILLE   = 4;
+    static final int OBJECTIF = 2048;
+}
