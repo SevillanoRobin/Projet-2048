a6566cf054ca390a9dff2c5403dbea1f42ba6f49
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-09-26 13:26:35 +0200

    4- Add Case's constructors.

--- src/Case.java
+++ src/Case.java
@@ -4,4 +4,21 @@ public class Case implements Parametres {
     private int value;
 
     private Grille grille;
+
+    public Case( int _x, int _y, int _value ) {
+        if ( _x < 0 || _x >= Parametres.TAILLE ) {
+            throw new IllegalArgumentException( "Case: x has a wrong value." );
+        }
+        if ( _y < 0 || _y >= Parametres.TAILLE ) {
+            throw new IllegalArgumentException( "Case: y has a wrong value." );
+        }
+
+        this.x = _x;
+        this.y = _y;
+        this.value = _value;
+    }
+
+    public Case( int[] _coors, int _value ) {
+        this( _coors[0], _coors[1], _value );
+    }
 }
