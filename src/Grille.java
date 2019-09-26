b77c1b917e2dfefbbf52eb77607c5cc09ff6d130
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-09-26 13:57:01 +0200

    5 - Add Grille's attributes.

--- src/Grille.java
+++ src/Grille.java
@@ -8,10 +8,17 @@
  *      - Sevillano Robin
  */
 
+import java.util.HashSet;
+
 /**
  * Classe Grille.
  * <p>
  * Represente une grille de jeu 2048, composée de {@link Case cellules}.
  */
 public class Grille implements Parametres {
+    /** La plus grande valeur au sein de la grille. */
+    private int valeurMax;
+
+    /** Ensemble des {@link Case cellules} de la grille. */
+    private HashSet<Case> grille;
 }
