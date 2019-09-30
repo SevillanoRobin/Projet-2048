d66f3e2751b6b4e8c8cd90b0b18cd757267bac45
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-09-30 19:27:33 +0200

    16 - Add Grille's partieFinie(). +Test

--- test/GrilleTest.java
+++ test/GrilleTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GrilleTest {
     private Grille grille;
@@ -11,7 +11,7 @@ class GrilleTest {
         this.grille = new Grille();
     }
 
-    private void furnishValues() {
+    private void furnishValues_toString() {
         this.grille.getGrille().add( new Case( 0, 0, 4 ) );
         this.grille.getGrille().add( new Case( 1, 2, 4 ) );
     }
@@ -19,7 +19,7 @@ class GrilleTest {
     @Test
     void testToString() {
         if ( Parametres.TAILLE == 4 ) {
-            this.furnishValues();
+            this.furnishValues_toString();
             String expected = "[4, 0, 0, 0]\n" +
                               "[0, 0, 0, 0]\n" +
                               "[0, 4, 0, 0]\n" +
@@ -27,5 +27,55 @@ class GrilleTest {
 
             assertEquals( expected, "" + this.grille, "The results are not corresponding." );
         }
+    }
+
+    private void furnishValues_finish() {
+        for ( int i = 0; i < Parametres.TAILLE; i++ ) {
+            for ( int j = 0; j < Parametres.TAILLE; j++ ) {
+                int value;
+
+                if ( i == j ) {
+                    value = 4;
+                } else if ( i % 2 == 0 ) {
+                    value = j % 2 == 0 ? 4 : 2;
+                } else {
+                    value = j % 2 == 0 ? 2 : 4;
+                }
+
+                Case _case = new Case( i, j, value );
+                _case.setGrille( this.grille );
+
+                this.grille.getGrille().add( _case );
+            }
+        }
+    }
+
+    @Test
+    void partieFinie_finished() {
+        this.furnishValues_finish();
+
+        assertTrue( this.grille.partieFinie(), "The grid is considered as not finished. " + this.grille.toString() );
+    }
+
+    @Test
+    void partieFinie_notFinished_notFilled() {
+        this.furnishValues_finish();
+        Case _case = new Case( 0, 0, 2 );
+        _case.setGrille( this.grille );
+        this.grille.getGrille().remove( _case );
+
+        assertFalse( this.grille.partieFinie(), "The grid is considered as finished. \n" + this.grille.toString() );
+    }
+
+    @Test
+    void partieFinie_notFinished_hasSameValue() {
+        this.furnishValues_finish();
+
+        Case _case = new Case( 0, 0, 2 );
+        _case.setGrille( this.grille );
+        this.grille.getGrille().remove( _case );
+        this.grille.getGrille().add( _case );
+
+        assertFalse( this.grille.partieFinie(), "The grid is considered as finished.  \n" + this.grille.toString() );
     }
 }
