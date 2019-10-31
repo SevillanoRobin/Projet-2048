e4ea366353d80fc0ee001351d191ef84205b64ca
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-11-01 00:07:52 +0100

    Tweaks the Main class.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,46 @@
+/*
+ * Copyright (c) 01/11/2019
+ *
+ * Auteurs :
+ *      - Behm Guillaume
+ *      - Claudel Adrien
+ *      - Richez Guillaume
+ *      - Sevillano Robin
+ */
+
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.util.Arrays;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 400, 400);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void main(String[] args) {
+        if (Arrays.asList(args).contains("text")) {
+            Main_Terminal.main(args);
+        } else {
+            launch(args);
+        }
+    }
+
+    @Override
+    public void stop() {
+        System.exit(0);
+    }
 }
