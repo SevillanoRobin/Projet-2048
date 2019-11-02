f567a5f2c9e012cb6f4183816d9a06035c762cdd
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-11-02 18:17:37 +0100

    fixup! Tweaks the Main_Terminal class.

--- src/application/Main_Terminal.java
+++ src/application/Main_Terminal.java
@@ -17,26 +17,28 @@ import java.util.Scanner;
 
 public class Main_Terminal implements Parametres {
 
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-		Grids g = new Grids();
-		
-		Scanner sc = new Scanner(System.in);
+        Grids g = new Grids();
 
-		System.out.println(g);
-		while (!g.victory() && !g.lose()) {
+        Scanner sc = new Scanner(System.in);
 
-			System.out.println("Déplacer vers la Droite (d), Gauche (q), Haut (h), Bas (b), Devant (r) ou Derrier (f) ?");
-			String s = sc.nextLine();
-			if (!(s.equals("d") || s.equals("right") ||
-					s.equals("q") || s.equals("left") ||
-					s.equals("z") || s.equals("up") ||
-					s.equals("s") || s.equals("down") ||
-					s.equals("r") || s.equals("front") ||
-					s.equals("f") || s.equals("back"))) {
-				System.out.println("Vous devez écrire (d) pour Droite, (q) pour Gauche, (z) pour Haut, (s) pour Bas, (r) pour Devant ou (f) pour Derrier");
-			} else {
-				int direction;
+        System.out.println(g);
+        while (!g.victory() && !g.lose()) {
+
+            System.out.println(
+                    "Déplacer vers la Droite (d), Gauche (q), Haut (h), Bas (b), Devant (r) ou Derrier (f) ?");
+            String s = sc.nextLine();
+            if (!(s.equals("d") || s.equals("right") ||
+                  s.equals("q") || s.equals("left") ||
+                  s.equals("z") || s.equals("up") ||
+                  s.equals("s") || s.equals("down") ||
+                  s.equals("r") || s.equals("front") ||
+                  s.equals("f") || s.equals("back"))) {
+                System.out.println(
+                        "Vous devez écrire (d) pour Droite, (q) pour Gauche, (z) pour Haut, (s) pour Bas, (r) pour Devant ou (f) pour Derrier");
+            } else {
+                int direction;
                 switch (s) {
                     case "d":
                     case "right":
@@ -63,10 +65,10 @@ public class Main_Terminal implements Parametres {
                         break;
                 }
 
-				g.move(direction);
-			}
-		}
-		sc.close();
-		System.exit(0);
-	}
+                g.move(direction);
+            }
+        }
+        sc.close();
+        System.exit(0);
+    }
 }
