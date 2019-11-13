d352f41381f0b97ea2fb55a684d615b7d6a0bf64
Prometheos2 <45640841+Prometheos2@users.noreply.github.com>
2019-11-13 17:33:15 +0100

    Tweak the Grids class, without affecting the features.

--- src/model/Grids.java
+++ src/model/Grids.java
@@ -1,9 +1,20 @@
+/*
+ * Copyright (c) 15/11/2019
+ *
+ * Auteurs :
+ *      - Behm Guillaume
+ *      - Claudel Adrien
+ *      - Richez Guillaume
+ *      - Sevillano Robin
+ */
+
 package model;
 
-public class Grids extends Movable{
+import static model.Parametres.*;
 
-	private Grid grids[];
-	
+public class Grids {
+
+    private Grid[] grids;
 
 	/**
 	 * constructeur de la matrice 3D
@@ -17,6 +28,34 @@ public class Grids extends Movable{
 		}
 	}
 
+    /**
+     * Accesseur de l'attribut {@code grids} (tableau de {@link Grid grille}).
+     *
+     * @return les grilles.
+     */
+    public Grid[] getGrids() {
+        return this.grids;
+    }
+
+    /**
+     * Accesseur des éléments de l'attribut {@code grids} (tableau de {@link Grid grille}).
+     *
+     * @param _ind Indice de la grille dans le tableau.
+     *
+     * @return la grille trouvée.
+     */
+    public Grid getGrid(int _ind) {
+        return this.grids[_ind];
+    }
+
+    /**
+     * Modificateur de l'attribut {@code grids} (tableau de {@link Grid grille}).
+     *
+     * @param _gs Nouvelle valeur de l'attribut.
+     */
+    private void setGrids(Grid[] _gs) {
+        this.grids = _gs;
+    }
 
 	/**
 	 * Meilleur score
@@ -36,15 +75,15 @@ public class Grids extends Movable{
 	 * @return boolean
 	 */
 	public boolean lose() {
-		
-		Grid tamp[] = reorganization(this.grids);
+
+        Grid[] tamp = reorganization(this.grids);
 		for(Grid g : tamp) {
 			if(!g.lose())
 				return false;
 		}
 		//on remet la matrice dans le bon sens
 		setGrids(reorganization(reorganization(reorganization(tamp))));
-		
+
 		for(Grid g : grids) {
 			if(!g.lose())
 				return false;
@@ -52,14 +91,15 @@ public class Grids extends Movable{
 
 		System.out.println(this);
 		System.out.println("La partie est finie. Votre score est " + this.best());
-		
+
 		return true;
 	}
 
 
 	/**
-	 * La partie est gagné
-	 * @return 
+     * Détermine si la partie est gagnée.
+     *
+     * @return {@code true} si la partie est déterminée comme gagnée.
 	 */
 	public boolean victory() {
 		if(best() >= GOAL) {
@@ -75,17 +115,17 @@ public class Grids extends Movable{
 	 * Gere les depacements des grilles
 	 */
 	public boolean move(int _d) {
-		boolean verif[] = {false, false, false};
+        boolean[] verif = { false, false, false };
 
 		for(int index  = 0; index < SIDE; index++) {
 			if(_d == FRONT) {
-				Grid tamp[] = reorganization(this.grids);
+                Grid[] tamp = reorganization(this.grids);
 				verif[index] = tamp[index].move(DOWN);
 				//on remet la matrice dans le bon sens
 				setGrids(reorganization(reorganization(reorganization(tamp))));
 			}
 			else if(_d == BACK) {
-				Grid tamp[] = reorganization(this.grids);
+                Grid[] tamp = reorganization(this.grids);
 				verif[index] = tamp[index].move(UP);
 				//on remet la matrice dans le bon sens
 				setGrids(reorganization(reorganization(reorganization(tamp))));
@@ -104,16 +144,12 @@ public class Grids extends Movable{
 	}
 
 
-	private void setGrids(Grid[] _gs) {
-		this.grids =_gs;
-	}
-
-
 	public String toString() {
-		String s = new String();
-		for(Grid g : grids)
-			s += g.toString();
-		return s;
+        StringBuilder s = new StringBuilder();
+        for (Grid g : grids) {
+            s.append(g.toString());
+        }
+        return s.toString();
 	}
 
 
@@ -141,8 +177,7 @@ public class Grids extends Movable{
 			}
 		}
 
-		Grid[] result = {new Grid(result1), new Grid(result2), new Grid(result3)};
-		return result;
+        return new Grid[] { new Grid(result1), new Grid(result2), new Grid(result3) };
 	}
-	
+
 }
