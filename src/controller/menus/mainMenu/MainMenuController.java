20d023caf7784a2807c19add4d3eba5d18d64002
Fork my Git <45640841+Prometheos2@users.noreply.github.com>
2019-12-05 21:53:21 +0100

    Create a dialog box factory for the main menu +JavaDoc.

--- src/controller/menus/mainMenu/MainMenuController.java
+++ src/controller/menus/mainMenu/MainMenuController.java
@@ -10,9 +10,8 @@
 
 package controller.menus.mainMenu;
 
-import javafx.event.ActionEvent;
+import controller.DialogBoxFactory;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
@@ -23,12 +22,33 @@ import java.util.ResourceBundle;
 
 /**
  * Contrôleur associé au menu principal.
+ * <p>
+ * Le menu est principalement composé de quatre boutons :
+ *      - Un bouton "Nouvelle Partie", lançant une partie <i>from scratch</i>. [non-implémenté]
+ *      - Un bouton "Continuer une Partie", lançant une partie depuis une sauvegarde. [non-implémenté]
+ *      - Un bouton "Options", pour changer quelques paramètres. [non-implémentés]
+ *      - Un bouton "Quitter", lançant une requête pour fermer l'application.
+ * <p>
+ * Comporte également un attribut pour le {@link Stage} afin de lancer soi-même des requêtes de fermetures,
+ * ainsi qu'une {@link DialogBoxFactory fabrique à boîte de dialogue}, pour simplifier la lisibilité de la classe, et
+ * séparer les responsabilités selon un <i>design pattern</i> de fabrique.
+ * <p>
+ * Ces attributs exigent des éléments créés par les classes chargeant le FXML, qui doivent donc être transférés,
+ * au moyen des méthodes {@link MainMenuController#initStage(Stage)} et
+ * {@link MainMenuController#initDialogFactory(ResourceBundle)} respectivement.
  */
 public class MainMenuController {
 
+    /** Fabrique à boîte de dialogue associée à ce contrôleur. */
+    private DialogBoxFactory dialogFactory;
+
+    /** Bouton "Nouvelle Partie". */
     @FXML private Button newGameButton;
+    /** Bouton "Charger une Partie". */
     @FXML private Button continueGameButton;
+    /** Bouton "Options". */
     @FXML private Button settingsButton;
+    /** Bouton "Quitter". */
     @FXML private Button quitButton;
 
     /** Stage associé à ce contrôleur. */
@@ -37,9 +57,13 @@ public class MainMenuController {
     private ResourceBundle bundle;
 
     /**
-     * Invoquée à la fin de l'initialisation de la {@link javafx.scene.Scene scène}.
+     * Actions effectuées durant le {@link javafx.fxml.FXMLLoader chargement de la vue FXML}.
      * <p>
-     * Permet de désactiver les boutons n'ayant pas encore d'implémentation.
+     * <b>Ne doit pas avoir de paramètres afin de pouvoir être lancée automatiquement.</b>
+     * <p>
+     * Permet pour l'instant de désactiver les boutons n'ayant pas encore d'implémentation.
+     *
+     * @see javafx.fxml.FXMLLoader
      */
     @FXML
     void initialize() {
@@ -49,13 +73,23 @@ public class MainMenuController {
     }
 
     /**
-     * Permet d'associer le {@link Stage stage} initialisé dans {@link application.Main}.
+     * Transfère l'attribut {@link Stage} depuis la classe accèdant au menu principal
+     * (par exemple, {@link application.Main}).
+     *
      * <p>
-     * Associe également un protocole pour s'occuper des requêtes de fermeture ({@link MainMenuController#onQuit()}.
+     * Modifie la propriété <i>onCloseRequest</i> du {@link Stage} afin d'appeler {@link
+     * MainMenuController#isCloseable()} et de devoir obtenir une validation avant de fermer l'application.
      *
      * @param _stage Le {@link Stage stage} associé à ce contrôleur.
+     *
+     * @throws IllegalStateException
+     *         Lancée si le {@link Stage} existe déjà à l'appel de cette méthode. <br>
+     *         N'est pas censée être rattrapée, étant un problème de programmation.
      */
     public void setStage(Stage _stage) {
+        if (this.stage != null) {
+            throw new IllegalStateException("The stage already exists");
+        }
         this.stage = _stage;
 
         this.stage.setOnCloseRequest(event -> {
@@ -68,33 +102,58 @@ public class MainMenuController {
     }
 
     /**
-     * Permet d'associer le {@link ResourceBundle pack de ressources} initialisé dans {@link application.Main}.
+     * Transfère une instance {@link ResourceBundle} vers l'instance {@link DialogBoxFactory} afin d'obtenir des
+     * boîtes de dialogues changeantes selon la langue de l'application.
+     * <p>
+     * Devrait être appelée par la classe qui a créée la dite instance (par exemple, {@link application.Main}).
      *
      * @param _bundle Le {@link ResourceBundle pack de ressources} associé à ce contrôleur.
+     *
+     * @throws IllegalArgumentException
+     *         Lancée si le {@link ResourceBundle pack} existe déjà à l'appel de cette méthode et ne change pas de
+     *         langue. <br>
+     *         N'est pas censée être rattrapée, étant un problème de programmation.
+     * @see DialogBoxFactory
      */
     public void setBundle(ResourceBundle _bundle) {
-        this.bundle = _bundle;
-    }
-
-    @FXML
-    private void onNewGame(ActionEvent _actionEvent) {
-        // TODO: Auto-generated stub.
-    }
-
-    @FXML
-    private void onContinue(ActionEvent _actionEvent) {
-        // TODO: Save feature not yet implemented.
-    }
-
-    @FXML
-    private void onSettings(ActionEvent _actionEvent) {
-        // TODO: Auto-generated stub.
+        if (dialogFactory == null) {
+            this.dialogFactory = new DialogBoxFactory(_bundle);
+        } else {
+            this.dialogFactory.setBundle(_bundle);
+        }
     }
 
     /**
-     * Invoquée en cliquant sur le bouton de fermeture ("quitter").
+     * [Non-implémentée] Action correspondante au bouton "Nouvelle Partie".
+     */
+    @FXML
+    private void onNewGame() {
+        // TODO: Implement a game interface.
+    }
+
+    /**
+     * [Non-implémentée] Action correspondante au bouton "Continuer une partie".
+     */
+    @FXML
+    private void onContinue() {
+        // TODO: Implement save & load features.
+    }
+
+    /**
+     * [Non-implémentée] Action correspondante au bouton "Options".
+     */
+    @FXML
+    private void onSettings() {
+        // TODO: Implement setting features.
+    }
+
+    /**
+     * Action correspondante au bouton "Quitter".
      * <p>
-     * Envoit une requête de fermeture qui peut ainsi être annulée ou confirmée.
+     * Envoit une requête de fermture au {@link Stage}, qui peut ensuite être validée ou annulée, au travers de
+     * la boîte de dialogue créée par la méthode {@link MainMenuController#isCloseable()}.
+     *
+     * @see javafx.scene.control.Alert la classe de la boîte de dialogue qui sera créée
      */
     @FXML
     private void onQuit() {
@@ -102,20 +161,21 @@ public class MainMenuController {
     }
 
     /**
-     * Invoquée lors d'une demande de fermeture de la fenêtre.
+     * Affiche une {@link javafx.scene.control.Alert boîte de dialogue} pour confirmer la fermeture de la fenêtre,
+     * et interprête ses résultats.
      * <p>
-     * Permet de bloquer la fermeture de l'application si non-voulue.
+     * Invoquée lors d'une demande de fermeture de la fenêtre, par la propriété <i>onCloseRequest</i> du {@link Stage}.
      * <p>
-     * Ne fonctionne pas avec la croix de fermeture.
+     * Permet de pouvoir bloquer la fermeture de l'application si non-voulue.
      *
-     * @return {@code true} si la boîte de dialogue est fermée avec le bouton "OK".
+     * @return {@code true} si la boîte de dialogue est fermée avec le bouton "OK", {@code false} autrement.
+     *
+     * @see DialogBoxFactory
+     * @see javafx.scene.control.Alert
      */
     @FXML
     private boolean isCloseable() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, this.bundle.getString("exit_confirmation.msg"));
-        alert.setTitle(this.bundle.getString("exit_confirmation.window"));
-
-        Optional<ButtonType> result = alert.showAndWait();
+        Optional<ButtonType> result = this.dialogFactory.CloseRequestDialog().showAndWait();
         return result.isPresent() && result.get().equals(ButtonType.OK);
     }
 }
